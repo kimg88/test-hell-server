b8b0ad4901ef051b5322bf656c29fb6d4412809b
김기성 <hellplus@ziptoss.com>
2020-10-28 19:08:09 +0900

    myTest.js

---
+++ myTest.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { get } = require('./router/loading.js');
+const router = express.Router();
+const loading = require('./router/loading.js');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3003;
+
+
+
+
+
+
+app.use('/loading', loading);
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=> {
+    res.send('hello world my world');
+})
+
+
+
+app.listen(port, ()=> {
+    console.log('server start port number:', port);
+})
