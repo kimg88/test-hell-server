43853bfdc23f751f6560dc0b6bb9f90fcc7a73d1
김기성 <hellplus@ziptoss.com>
2020-10-29 11:54:58 +0900

    simple test http method

--- router/loading.js
+++ router/loading.js
@@ -2,6 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 
+// CRUD => create, read, update, delete
+// Rest API
+let db = [
+    {
+        id: 0,
+        name: 'kim',
+        age: 30
+    },
+    {
+        id: 1,
+        name: 'park',
+        age: 30
+    },
+];
+/*
 router.get('/v1',(req,res)=>{
     return res.json({
         name: 'a',
@@ -11,18 +26,39 @@ router.get('/v1',(req,res)=>{
         boolean: true
     });
 });
+router.get('/v2/:id',(req,res)=>{
+    return res.send(`v2 ${req.params.id}`);
+});
+*/
 
-
-router.get('/v1/:id',(req,res)=>{
+router.get('/v1',(req,res)=>{
     return res.json({
-        name: 'a',
-        password: 'bc',
-        gender: 'male'
+        result: db
     });
 });
 
-router.get('/v2/:id',(req,res)=>{
-    return res.send(`v2 ${req.params.id}`);
+router.get('/v1/:id',(req,res)=>{
+    const search = db.filter((item)=>{
+        item.id === req.params.id
+    });
+
+    return res.json({
+        result: search
+    });
+});
+
+router.put('/v1',(req,res)=>{
+    const id = req.body.id;
+    const changedName = req.body.name;
+    db = db.map(item=>{
+        if(item.id === id){
+            item.name = changedName;
+        }
+        return item;
+    })
+    return res.json({
+        result: db
+    });
 });
 
 module.exports = router;
